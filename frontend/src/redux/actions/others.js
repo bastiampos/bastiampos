c2abdb62922152fe2fa72778b4afa9a75b799ec4
Sebastian Campos <bastiampos.uni@gmail.com>
2021-12-10 11:13:11 -0300

    submodules

--- frontend/src/redux/actions/others.js
+++
@@ -1,7 +0,0 @@
-// import axios from 'axios'
-
-const authentication = {
-   changeLanguage: (language) => (dispatch, getState) => dispatch({ type: 'CHANGE_LANGUAGE', payload: language }),
-}
-
-export default authentication
